898425b168a17b5b9d5ba3e9f3cbc960d746ef05
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-20 12:04:18 +0200

    Ajout de commentaire

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-session_start();
+session_start();//Démarre le système de session
 
+//Déclaration d'une variable de session 'userAgent'
+//session_id s'il n'est pas défini génère un id aléatoire
+//C'est une fonction préétabli par PHP
 $_SESSION['userAgent'] = session_id();
-$_SESSION['serverName'] = 'serverDeBGMaGle';
+$_SESSION['serverName'] = 'serverDeBGMaGle';//Déclaration d'une variable de session 'serverName'
 ?>
 <!DOCTYPE html>
 <html lang="foreach" dir="ltr">
@@ -12,9 +15,10 @@ $_SESSION['serverName'] = 'serverDeBGMaGle';
   </head>
   <body>
       <p>
-        Votre User Agent est : <?php echo $_SESSION['userAgent']; ?><br />
-        Votre adresse IP est: <?php echo $_SERVER['REMOTE_ADDR']; ?> <br />
-        Et votre serveur s'appelle => <?php echo $_SESSION['serverName']; ?>
+        Votre User Agent est : <?php echo $_SESSION['userAgent']; ?><br /><!--Affichage de la valeur des valeurs de session-->
+        Votre adresse IP est: <?php echo $_SERVER['REMOTE_ADDR']; ?> <br /><!--Affichage de la valeur des valeurs de session-->
+        <!--$_SERVER['REMOTE_ADDR'] est une fonction pré-établie par PHP pour connaître l'adresse IP-->
+        Et votre serveur s'appelle => <?php echo $_SESSION['serverName']; ?><!--Affichage de la valeur des valeurs de session-->
       </p>
   </body>
 </html>
